dca32ddea089721b84e3350128e3514fd9450607
Muhammad Kassar <36678834+mk360@users.noreply.github.com>
2025-04-03 14:57:07 +0200

    allow for multiple battles for the same player

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,21 @@ import { team1, team2 } from "./debug-world";
 import shortid from "short-unique-id";
 
 const uid = new shortid({ length: 10 });
+
+/**
+ * Maps room IDs (game IDs) to their matching GameWorlds
+ */
 const GAME_WORLDS_MAP: { [k: string]: GameWorld } = {};
-const GAME_WORLDS_ID_MAP: { [k: string]: GameWorld } = {};
-const UUID_BY_ID: { [k: string]: string } = {};
+
+/**
+ * Maps room IDs (game IDs) to socket IDs inside that room
+ */
 const SOCKETS_BY_ROOM: { [k: string]: string[] } = {};
+
+/**
+ * Map socket IDs to the rooms they are in.
+ */
+const ROOMS_BY_SOCKETS: { [k: string]: string[] } = {};
 
 function groupBy<T>(iterable: T[], filter: (element: T) => string) {
     const group: { [k: string]: T[] } = {};
@@ -65,24 +76,37 @@ function parseEntities(world: GameWorld) {
 };
 
 io.on("connection", (socket) => {
-    socket.on("loading-complete", ({ uuid }) => {
-        const gameWorld = GAME_WORLDS_MAP[uuid];
+    socket.on("loading-complete", ({ roomId }) => {
+        const gameWorld = GAME_WORLDS_MAP[roomId];
+        console.log("gameworld check", !!gameWorld)
+        socket.join(roomId);
         if (!gameWorld) {
 
         } else {
             const teamIds = gameWorld.state.teamIds;
-            socket.emit("allow-control", ({ ids: teamIds, id: uuid }));
+            socket.emit("allow-control", ({ ids: teamIds, id: socket.handshake.auth.uuid }));
         }
     });
 
     socket.on("create-session", ({ uuid }) => {
         const newId = uid.randomUUID();
         socket.join(newId);
-        UUID_BY_ID[socket.id] = uuid;
+
         SOCKETS_BY_ROOM[newId] = [uuid];
+        ROOMS_BY_SOCKETS[uuid] = [newId];
         socket.emit("confirm", "Your session has been created. Please share the ID <code>" + newId + "</code> with your opponent.");
     }).on("disconnect", () => {
-        delete UUID_BY_ID[socket.id];
+        const uuid = socket.handshake.auth.uuid;
+        const rooms = ROOMS_BY_SOCKETS[uuid] ?? [];
+        for (let room of rooms) {
+            socket.leave(room);
+            SOCKETS_BY_ROOM[room].splice(SOCKETS_BY_ROOM[room].indexOf(uuid), 1);
+            if (SOCKETS_BY_ROOM[room].length === 0) {
+                delete SOCKETS_BY_ROOM[room]; // everybody left, room doesn't exist anymore
+                delete GAME_WORLDS_MAP[room]; // delete game world. TODO : check if gameworld stays in memory or gets Garbage Collected
+            }
+        }
+        delete ROOMS_BY_SOCKETS[uuid];
     }).on("join", ({ roomId, uuid }) => {
         if (!SOCKETS_BY_ROOM[roomId]) {
             socket.emit("error", "This session does not exist.");
@@ -92,7 +116,7 @@ io.on("connection", (socket) => {
             socket.emit("error", "This session is full.");
             return;
         }
-        UUID_BY_ID[socket.id] = uuid;
+
         SOCKETS_BY_ROOM[roomId].push(uuid);
         socket.join(roomId);
         io.in(roomId).fetchSockets().then(() => {
@@ -101,26 +125,25 @@ io.on("connection", (socket) => {
                 team2: SOCKETS_BY_ROOM[roomId][1],
                 trackChanges: true,
             });
-            GAME_WORLDS_ID_MAP[newWorld.id] = newWorld;
             newWorld.generateMap();
             newWorld.initiate({
                 team1: team1,
                 team2: team2,
             });
             newWorld.startTurn();
-            GAME_WORLDS_MAP[SOCKETS_BY_ROOM[roomId][0]] = newWorld;
-            GAME_WORLDS_MAP[SOCKETS_BY_ROOM[roomId][1]] = newWorld;
-            io.in(roomId).emit("join-session", newWorld.id);
+
+            GAME_WORLDS_MAP[roomId] = newWorld;
+            io.in(roomId).emit("join-session", roomId);
         });
     });
-    socket.on("ready", ({ uuid }) => {
-        const world = GAME_WORLDS_MAP[uuid];
+    socket.on("ready", ({ roomId }) => {
+        const world = GAME_WORLDS_MAP[roomId];
         socket.emit("response", world.state.lastChangeSequence);
     });
 
     // il faudra trouver un moyen de batch plusieurs responses de sockets
-    socket.on("request preview movement", ({ uuid, unitId }: { uuid: string, unitId: string }) => {
-        const world = GAME_WORLDS_MAP[uuid];
+    socket.on("request preview movement", ({ unitId, roomId }: { unitId: string, roomId: string }) => {
+        const world = GAME_WORLDS_MAP[roomId];
         const { movementTiles, attackTiles, warpTiles, targetableTiles, effectiveness, assistTiles } = world?.getUnitMovement(unitId);
         let movementArray: number[] = [];
         movementTiles.forEach((comp) => {
@@ -165,12 +188,11 @@ io.on("connection", (socket) => {
         });
     }).on("request confirm movement", (payload: {
         unitId: string,
-        uuid: string,
         roomId: string,
         x: number,
         y: number
     }) => {
-        const world = GAME_WORLDS_MAP[payload.uuid];
+        const world = GAME_WORLDS_MAP[payload.roomId];
         if (world.previewUnitMovement(payload.unitId, payload)) {
             const actionEnd = world.moveUnit(payload.unitId, payload, true);
             io.in(payload.roomId).emit("response confirm movement", {
@@ -190,7 +212,7 @@ io.on("connection", (socket) => {
                 y: oldPosition!.y,
             });
         }
-    }).on("request preview battle", (payload: { unit: string, uuid: string, x: number, y: number, position: MapCoords, path: MapCoords[] }) => {
+    }).on("request preview battle", (payload: { unit: string, uuid: string, x: number, y: number, position: MapCoords, path: MapCoords[], roomId: string }) => {
         const gameWorld = GAME_WORLDS_MAP[payload.uuid];
         const preview = gameWorld.previewCombat(payload.unit, { x: payload.x, y: payload.y }, payload.position, payload.path);
         socket.emit("response preview battle", preview);
@@ -215,24 +237,26 @@ io.on("connection", (socket) => {
         const world = GAME_WORLDS_MAP[payload.uuid];
         const combatActions = world.runCombat(payload.unitId, payload.attackerCoordinates, { x: payload.x, y: payload.y }, payload.path);
         io.in(payload.roomId).emit("response", combatActions);
-    }).on("request preview assist", (payload: { source: string, uuid: string, sourceCoordinates: MapCoords, targetCoordinates: MapCoords }) => {
+    }).on("request preview assist", (payload: { source: string, uuid: string, sourceCoordinates: MapCoords, targetCoordinates: MapCoords, roomId: string }) => {
         const world = GAME_WORLDS_MAP[payload.uuid];
         const preview = world.previewAssist(payload.source, payload.targetCoordinates, payload.sourceCoordinates);
-        socket.emit("response preview assist", preview);
+        socket.in(payload.roomId).emit("response preview assist", preview);
     }).on("request confirm assist", (payload: { source: string, uuid: string, targetCoordinates: MapCoords, sourceCoordinates: MapCoords, roomId: string }) => {
         const world = GAME_WORLDS_MAP[payload.uuid];
         const assistActions = world.runAssist(payload.source, payload.targetCoordinates, payload.sourceCoordinates);
         io.in(payload.roomId).emit("response", assistActions);
-    }).on("request enemy range", (payload: { sideId: string, worldId: string }) => {
+    }).on("request enemy range", (payload: { uuid: string, roomId: string }) => {
 
     }).on("request update", (payload: { uuid: string, roomId: string }) => {
         const world = GAME_WORLDS_MAP[payload.uuid];
-        const updatedEntities = parseEntities(world);
-        io.in(payload.roomId).emit("update-entities", updatedEntities);
-    }).on("request end turn", ({ uuid }: { uuid: string }) => {
+        if (world) {
+            const updatedEntities = parseEntities(world);
+            io.in(payload.roomId).emit("update-entities", updatedEntities);
+        }
+    }).on("request end turn", ({ uuid, roomId }: { uuid: string, roomId: string }) => {
         const world = GAME_WORLDS_MAP[uuid];
         const newTurn = world.startTurn();
-        io.in(world.id).emit("response", newTurn);
+        io.in(roomId).emit("response", newTurn);
     });
 });
 
@@ -258,12 +282,8 @@ app.get("/moveset", (req, res) => {
     res.end();
 });
 
-app.post("/create", (req, res) => {
-
-});
-
 app.get("/worlds/:id", (req, res) => {
-    const world = GAME_WORLDS_ID_MAP[req.params.id];
+    const world = GAME_WORLDS_MAP[req.params.id];
     if (world) {
         const heroes = parseEntities(world);
         res.status(200);
